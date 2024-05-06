8e509ec768264262ed7dcd3c3e3ca31e5f5cd8e4
ImproperIssues <114215797+ItziSpyder@users.noreply.github.com>
2024-05-05 21:56:25 -0700

    fixed config key error

--- src/main/java/io/github/itzispyder/improperui/config/ConfigKeyHolder.java
+++ src/main/java/io/github/itzispyder/improperui/config/ConfigKeyHolder.java
@@ -7,7 +7,7 @@ import java.util.function.Function;
 public interface ConfigKeyHolder {
 
     Function<Element, ConfigKey> ELEMENT_KEY_HOLDER = element -> {
-        String regex = "[a-zA-Z0-9.-]+\\.[a-zA-Z0-9.-]+:[a-zA-Z0-9.-]+";
+        String regex = "([a-zA-Z0-9_.-]+:)?[a-zA-Z0-9_.-]+\\.[a-zA-Z0-9_.-]+:[a-zA-Z0-9_.-]+";
         for (var s : element.classList)
             if (s.matches(regex))
                 return new ConfigKey(s);
