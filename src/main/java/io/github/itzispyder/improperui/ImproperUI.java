ee6e072cdddcad78aabb9877568459ea4e9b8495
ImproperIssues <114215797+ItziSpyder@users.noreply.github.com>
2024-05-07 22:29:13 -0700

    forgot to remove the testing binds

--- src/main/java/io/github/itzispyder/improperui/ImproperUI.java
+++ src/main/java/io/github/itzispyder/improperui/ImproperUI.java
@@ -1,20 +1,8 @@
 package io.github.itzispyder.improperui;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 
 public class ImproperUI implements ModInitializer {
-
-    public static final KeyBinding BIND = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "binds.improperui.menu",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_RIGHT_SHIFT,
-            "binds.improperui"
-    ));
 
     @Override
     public void onInitialize() {
@@ -22,11 +10,5 @@ public class ImproperUI implements ModInitializer {
                 "scripts/homescreen.ui",
                 "scripts/example.ui"
         );
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (BIND.wasPressed()) {
-                ImproperUIAPI.parseAndRunFile("improperui", "homescreen.ui");
-            }
-        });
     }
 }
